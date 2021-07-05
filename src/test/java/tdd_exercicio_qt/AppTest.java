7896ca6a6408530d863cfeddb39516ff29b016a1
ChrisACorrea <christopher.a.c@outlook.com>
2021-07-04 22:55:34 -0300

    Commit Inicial

---
+++ src/test/java/tdd_exercicio_qt/AppTest.java
@@ -0,0 +1,13 @@
+package tdd_exercicio_qt;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
